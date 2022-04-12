470d5ba6d7e415686be04b0c751a1e0e39d4dc6b
N0N4M3D-b <20wlsaud@gmail.com>
2022-04-13 01:09:40 +0900

    Add user api namespace

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_restx import Api
-from secret import *
+from dbinit import *
+from api.users import Users
+from api.users import Login
+from api.secret import *
 
+InitTable()
 app = Flask(__name__)
 api = Api(app)
 
+api.add_namespace(Users, '/users')
+api.add_namespace(Login, '/login')
+
 if __name__ == '__main__':
-    app.run(debug=True, host=HOST, port=PORT)
+    app.run(debug=True, host=app_host, port=app_port)
