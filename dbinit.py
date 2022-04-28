edbae88a7fc1424ce566ed7acd3c50607539048c
N0N4M3D-b <20wlsaud@gmail.com>
2022-04-28 23:12:00 +0900

    Fix user sp1 access denied

--- dbinit.py
+++ dbinit.py
@@ -1,12 +1,28 @@
 import pymysql
+import os
 from api.secret import *
 
 class InitTable:
     def __init__(self):
+        self.conn = pymysql.connect(host=db_host, port=db_port, user=db_root, password=db_root_password, db=mysql, charset='utf8')
+        self.db_cursor = self.conn.cursor()
+        self.create_project_user()
+        self.conn.commit()
+        self.conn.close()
+
         self.conn = pymysql.connect(host=db_host, port=db_port, user=db_user, password=db_password, db=db, charset='utf8')
         self.db_cursor = self.conn.cursor()
         self.check_user_table()
+        self.conn.commit()
         self.conn.close()
+
+
+    def create_project_user(self):
+        if self.db_cursor.execute('SELECT user FROM user WHERE user="sp1"'):
+            return
+        self.db_cursor.execute('CREATE DATABASE sp1 DEFAULT CHARACTER SET utf8')
+        self.db_cursor.execute('CREATE USER "sp1"@"%" IDENTIFIED BY "user_passwd"')
+        self.db_cursor.execute('GRANT ALL PRIVILEGES ON sp1.* TO "sp1"@"%"')
 
 
     def select_table_query(self, table_name):
